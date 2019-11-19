b67d2c44281392ba5908d36cf71cd2bbcaa456e6
suhao1 <suhao1@vipkid.com.cn>
2019-11-19 15:00:58 +0800

    s

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
-    //- main.center.bg
-    //-     a-form(:form="loginForm")
-    //-         a-form-item(label="邮箱", v-bind="layout")
-    //-             a-input( v-decorator="['email', {rules: [{ required: true, message: '请输入邮箱'}] }]", placeholder="请输入邮箱")
-    //-         a-form-item(label="密码", v-bind="layout")
-    //-             a-input-password( v-decorator="['pwd', {rules: [{ required: true, message: '请输入密码'}]}]", placeholder="请输入密码")
-    //-         a-form-item.flexSpace
-    //-             a-button.mr20(type="primary", @click="handleSubmit('login')") 登录
-    //-             a-button(type="primary", @click="handleSubmit('signin')") 注册
+    main.center.bg
+        a-form(:form="loginForm")
+            a-form-item(label="邮箱", v-bind="layout")
+                a-input( v-decorator="['email', {rules: [{ required: true, message: '请输入邮箱'}] }]", placeholder="请输入邮箱")
+            a-form-item(label="密码", v-bind="layout")
+                a-input-password( v-decorator="['pwd', {rules: [{ required: true, message: '请输入密码'}]}]", placeholder="请输入密码")
+            a-form-item.flexSpace
+                a-button.mr20(type="primary", @click="handleSubmit('login')") 登录
+                a-button(type="primary", @click="handleSubmit('signin')") 注册
     span  假装有表单
 </template>
 
@@ -17,43 +17,43 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Login extends Vue {
-  // loginForm: any;
-  // layout = {
-  //   labelCol: {
-  //     xs: {
-  //       span: 4
-  //     }
-  //   },
-  //   wrapperCol: {
-  //     xs: {
-  //       span: 18,
-  //       offset: 1
-  //     }
-  //   }
-  // };
+  loginForm: any;
+  layout = {
+    labelCol: {
+      xs: {
+        span: 4
+      }
+    },
+    wrapperCol: {
+      xs: {
+        span: 18,
+        offset: 1
+      }
+    }
+  };
 
-  // beforeCreate() {
-  //   this.loginForm = this.$form.createForm(this);
-  // }
-  // handleSubmit(handleType: "login" | "signin") {
-  //   console.log(handleType);
-  //   this.loginForm.validateFields((err: string, values: object) => {
-  //     if (!err) {
-  //       // 登录
-  //       if (handleType == "login") {
-  //         this.$http.post("/api/user/create", values).then(res => {
-  //           console.log(res);
-  //         });
-  //       }
-  //       // 注册
-  //       if (handleType == "signin") {
-  //         this.$http.post("/api/user/create", values).then(res => {
-  //           console.log(res);
-  //         });
-  //       }
-  //     }
-  //   });
-  // }
+  beforeCreate() {
+    this.loginForm = this.$form.createForm(this);
+  }
+  handleSubmit(handleType: "login" | "signin") {
+    console.log(handleType);
+    this.loginForm.validateFields((err: string, values: object) => {
+      if (!err) {
+        // 登录
+        if (handleType == "login") {
+          this.$http.post("/api/user/create", values).then(res => {
+            console.log(res);
+          });
+        }
+        // 注册
+        if (handleType == "signin") {
+          this.$http.post("/api/user/create", values).then(res => {
+            console.log(res);
+          });
+        }
+      }
+    });
+  }
 }
 </script>
 
